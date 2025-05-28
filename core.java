8f8820bdff005e2627453eed09c6b213eb65848c
Shreevarshinir0506 <121334511+Shreevarshinir0506@users.noreply.github.com>
2025-05-28 23:28:30 +0530

    Update core.java

--- core.java
+++ core.java
@@ -132,4 +132,94 @@
     }
 
 //Program 9:
+        package cts;
+        import java.util.*;
+        public class CTS {
+            public static void main(String[] args) {
+                Scanner sc= new Scanner(System.in);
+                System.out.print("Enter Mark: ");
+                int grade=sc.nextInt();
+                if(grade>=90 && grade<=100){
+                    System.out.println("Your Grade is : A");
+                }
+                else if(grade>=80 && grade<90){
+                    System.out.println("Your Grade is : B");
+                }
+                else if(grade>=70 && grade<80){
+                    System.out.println("Your Grade is : C");
+                }
+                else if(grade>=60 && grade<70){
+                    System.out.println("Your Grade is : D");
+                }
+                else{
+                    System.out.println("Your Grade is : F");
+                }
+            }
+        }
 
+//Program 10:
+        package cts;
+        import java.util.*;
+        public class CTS {
+            public static void main(String[] args) {
+                Scanner sc= new Scanner(System.in);
+                Random r=new Random();
+                int sys_guess=r.nextInt(100)+1;
+                System.out.println("SystemGuess:"+sys_guess);
+                int user_guess=0;
+                while(user_guess!=sys_guess){
+                    System.out.println("UserGuess: ");
+                    user_guess=sc.nextInt();
+                    if(user_guess<sys_guess){
+                        System.out.println("Your guess is too low");
+                    }
+                    else if(user_guess>sys_guess){
+                        System.out.println("Your guess is too high");
+                    }
+                    else{
+                        System.out.println("Congratulation,your guess is correct");
+                    }
+                }
+            }
+        }
+
+//Program 11:
+        package cts;
+        import java.util.*;
+        public class CTS {
+            public static void main(String[] args) {
+                Scanner sc= new Scanner(System.in);
+                System.out.println("Enter the number: ");
+                int n = sc.nextInt();
+                if (n<0){
+                    System.out.println("Invalid input");
+                }
+                else{
+                    long factorial=1;
+                    for(int i=1;i<=n;i++){
+                        factorial*=i;
+                    }
+                    System.out.println("Factorial of "+n+" is "+factorial);
+                }
+            }
+        }
+
+//Program 12:
+        package cts;
+        import java.util.*;
+        public class CTS {
+            public static int add(int a,int b){
+                return a+b;
+            }
+            public static double add(double a,double b){
+                return a+b;
+            }
+            public static int add(int a,int b,int c){
+                return a+b+c;
+            }
+            public static void main(String[] args) {
+                System.out.println("Addition of 2 integers:"+add(5,5));
+                System.out.println("Addition of 2 doubles:"+add(5.0,4.0));
+                System.out.println("Addition of 3 integers:"+add(5,5,5));
+            }
+        }
